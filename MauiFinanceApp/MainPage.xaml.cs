d9bb0cbc75c5dc6ab7b691c9daf5d3589dfacbaf
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-12-10 20:06:17 +0100

    design first page

--- MauiFinanceApp/MainPage.xaml.cs
+++ MauiFinanceApp/MainPage.xaml.cs
@@ -1,24 +1,11 @@
 ﻿namespace MauiFinanceApp;
 
-public partial class MainPage : ContentPage
+public partial class MainPage
 {
-	int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 }
 
---
+++ MauiFinanceApp/Pages/Login.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiFinanceApp.Pages;
+
+public partial class Login : ContentPage
+{
+	public Login()
+	{
+		InitializeComponent();
+	}
+}
