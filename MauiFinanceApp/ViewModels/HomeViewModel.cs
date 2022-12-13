16c7b875345997d6e39022f08eb840dfa6a82587
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-12-13 21:39:04 +0100

    minor change

---
+++ MauiFinanceApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CommunityToolkit.Mvvm.Input;
+
+namespace MauiFinanceApp.ViewModels;
+
+public partial class HomeViewModel : BaseViewModel
+{
+
+    [RelayCommand]
+    void NavigateToStatistic()
+    {
+        Shell.Current.GoToAsync("/Analytics",true);
+    }
+}
+
