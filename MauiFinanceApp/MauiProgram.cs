b4b8a73aa2ef5b9361d39bc72e6be2742780009a
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-12-14 13:43:10 +0100

    add new font, and re-ajust UI wallet

--- MauiFinanceApp/MauiProgram.cs
+++ MauiFinanceApp/MauiProgram.cs
@@ -17,6 +17,8 @@ public static class MauiProgram
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Unbounded-Bold.ttf", "UnboundBold");
+                fonts.AddFont("Unbounded-Light.ttf", "UnboundLight");
             })
             .UseMauiCommunityToolkit();
 
